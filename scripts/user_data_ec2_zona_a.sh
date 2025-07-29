962bb95eb07d8e9c72a45745807d94b90315e7a8
Junior Fernandes <crfjunior65@gmail.com>
2025-07-29 15:23:53 -0300

    Subindo IaaC

--- scripts/user_data_ec2_zona_a.sh
+++ scripts/user_data_ec2_zona_a.sh
@@ -36,3 +36,45 @@ sudo dnf install python3.11 -y
 sudo ln -sf /usr/bin/python3.11 /usr/bin/python3
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+cd /home/ec2-user
+
+#Instalar o mcp servers da aws
+git clone https://github.com/aws/mcp-servers.git
+cd mcp-servers
+uv pip install -r requirements.txt
+uv pip install aws-mcp-servers
+uv pip install aws-mcp-servers-cli
+uv pip install aws-mcp-servers-cli-aws
+uv pip install aws-mcp-servers-cli-aws-ec2
+uv pip install aws-mcp-servers-cli-aws-ec2-zona-a
+
+# Instalando jq
+sudo yum install jq -y
+
+#Instalando AWS CLI v2
+sudo yum install unzip -y
+curl -s https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip
+unzip awscliv2.zip
+sudo ./aws/install
+
+#Instalando AmazonQ CLI
+#https://docs.aws.amazon.com/amazonq/latest/qdeveloper-ug/command-line-installing-ssh-setup-autocomplete.html
+cd /home/ec2-user
+curl --proto '=https' --tlsv1.2 -sSf "https://desktop-release.q.us-east-1.amazonaws.com/latest/q-x86_64-linux.zip" -o "q.zip"
+unzip q.zip 
+
+#./q/install.sh
+#Confirm the following code in the browser
+#Code: XPBF-VKJT
+
+#Open this URL: https://view.awsapps.com/start/#/device?user_code=XPBF-VKJT
+#Device authorized
+#Logged in successfully
+
+#Gerar Par de Chaves
+#ssh-keygen
+
+#Instalando AmazonQ 
+
+sudo curl -s https://raw.githubusercontent.com/aws/amazonq-cli/main/install.sh | bash
